a78f59b3dd194c143ccc76285a8fce14b7e53379
ucrprogrammerFreddy <freddy.sanchezzamora@ucr.ac.cr>
2025-06-03 15:59:10 -0600

    Update app.js
    se cambvio el app.js

--- 01-traducciones-openai/app.js
+++ 01-traducciones-openai/app.js
@@ -105,6 +105,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en http://localhost:" + PORT);
+// app.listen(PORT, () => {
+//   console.log("Servidor corriendo en http://localhost:" + PORT);
+// });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
