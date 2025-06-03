e6c12027d10a196ac2d244f962ac50977e7e6a57
ucrprogrammerFreddy <freddy.sanchezzamora@ucr.ac.cr>
2025-06-03 16:04:02 -0600

    Update app.js
    se borro la direvccion cvarpeta local

--- 01-traducciones-openai/app.js
+++ 01-traducciones-openai/app.js
@@ -101,9 +101,9 @@ const translatedText = completion.choices[0].message.content;
 
 
 // Ruta explícita para index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // app.listen(PORT, () => {
 //   console.log("Servidor corriendo en http://localhost:" + PORT);
